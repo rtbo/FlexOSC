de2e0abf0252936c03a2f82a154dad1d7dd8e289
Remi Thebault <remi.thebault@gmail.com>
2020-04-13 11:06:17 +0200

    initial import

---
+++ app/src/main/java/org/rtbo/flexosc/MainActivity.kt
@@ -0,0 +1,17 @@
+package org.rtbo.flexosc
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.activity_main.view.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        connectBtn.setOnClickListener {
+            it.connectBtn
+        }
+    }
+}
