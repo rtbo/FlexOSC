3fddb09d261a009296e40fb461aba08f894137ed
Remi Thebault <remi.thebault@gmail.com>
2020-04-15 17:53:13 +0200

    Logging clean up

--- app/src/main/java/org/rtbo/flexosc/OscConnection.kt
+++ app/src/main/java/org/rtbo/flexosc/OscConnection.kt
@@ -30,19 +30,10 @@ class UdpOscConnection(params: ConnectionParams) : OscConnection(params) {
 
     override suspend fun sendMessage(msg: OscMessage) {
         val arr = msg.payload
-        val zer = arr[msg.address.length].toInt();
 
         withContext(Dispatchers.IO) {
-            Log.d("OSC", "sending message ${msg.address} on ${hostAddress.toString()}:$params.sendPort")
-            Log.d("OSC", "arr.size = ${arr.size} / address.length = ${msg.address.length}")
-            Log.d("OSC", "zero = ${arr[msg.address.length].toInt()}")
-            Log.d("OSC", "arr = ${toHex(arr)}")
-            Log.d("OSC", "msg.address = ${toHex(msg.address.toByteArray(Charsets.US_ASCII))}")
+            Log.d("OSC_MSG", "sending message ${msg.address} on ${hostAddress.toString()}:$params.sendPort")
             sendSocket.send(DatagramPacket(arr, arr.size, hostAddress, params.sendPort))
         }
     }
-}
-
-fun toHex(arr: ByteArray): String {
-    return arr.joinToString(", ") { String.format("%02X", it) }
 }
