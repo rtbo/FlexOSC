86d8a5722ad2b0be4df369cb4c94b1a831c88f2c
Remi Thebault <remi.thebault@gmail.com>
2020-04-16 10:34:57 +0200

    fix protocol

--- app/src/main/java/org/rtbo/flexosc/model/OscProtocol.kt
+++ app/src/main/java/org/rtbo/flexosc/model/OscProtocol.kt
@@ -6,6 +6,18 @@ import java.nio.ByteOrder
 sealed class OscAtomic {
     abstract val tagLen: Pair<Char, Int>
     abstract fun encode(buffer: ByteBuffer)
+
+    companion object {
+        fun decode(typeTag: Char, buffer: ByteBuffer): OscAtomic {
+            return when (typeTag) {
+                'i' -> OscInt(buffer.int)
+                'f' -> OscFloat(buffer.float)
+                's' -> OscString.decode(buffer)
+                'b' -> OscBlob.decode(buffer)
+                else -> throw Exception("Unsupported OSC atomic type: '$typeTag'")
+            }
+        }
+    }
 }
 
 data class OscInt(val value: Int) : OscAtomic() {
@@ -31,9 +43,9 @@ data class OscString(val value: String) : OscAtomic() {
         }
     }
 
-    override val tagLen = Pair('s', value.length + 1 + (value.length + 1) % 4)
+    override val tagLen = Pair('s', alignUp(value.length + 1))
     override fun encode(buffer: ByteBuffer) {
-        var numNulls = 1 + (value.length + 1) % 4
+        var numNulls = alignUp(value.length + 1) - value.length
         buffer.put(value.toByteArray(Charsets.US_ASCII))
         while ((numNulls--) > 0) {
             buffer.put(0.toByte())
@@ -53,7 +65,7 @@ data class OscString(val value: String) : OscAtomic() {
                 } else {
                     val numChars = buf.indexOfFirst { it.toInt() == 0 }
                     if (numChars != 0) {
-                        list.add(buf.sliceArray(IntRange(0, numChars)))
+                        list.add(buf.sliceArray(IntRange(0, numChars - 1)))
                         sz += numChars
                     }
                     break
@@ -72,16 +84,6 @@ data class OscString(val value: String) : OscAtomic() {
 
 data class OscBlob(val value: ByteArray) : OscAtomic() {
 
-    override fun encode(buffer: ByteBuffer) {
-        buffer.putInt(value.size)
-        buffer.put(value)
-        var numNulls = value.size % 4
-        while (numNulls > 0) {
-            buffer.put(0.toByte())
-            numNulls--
-        }
-    }
-
     override fun equals(other: Any?): Boolean {
         if (this === other) return true
         if (javaClass != other?.javaClass) return false
@@ -92,11 +94,20 @@ data class OscBlob(val value: ByteArray) : OscAtomic() {
 
         return true
     }
-
-    override val tagLen = Pair('b', 4 + value.size + value.size % 4)
-
     override fun hashCode(): Int {
         return value.contentHashCode()
+    }
+
+    override val tagLen = Pair('b', 4 + alignUp(value.size))
+
+    override fun encode(buffer: ByteBuffer) {
+        buffer.putInt(value.size)
+        buffer.put(value)
+        var numNulls = alignUp(value.size) - value.size
+        while (numNulls > 0) {
+            buffer.put(0.toByte())
+            numNulls--
+        }
     }
 
     companion object {
@@ -104,8 +115,7 @@ data class OscBlob(val value: ByteArray) : OscAtomic() {
             val len = buffer.int
             val value = ByteArray(len)
             buffer.get(value)
-            // align up
-            var numNulls = len % 4
+            var numNulls = alignUp(value.size) - value.size
             while (numNulls > 0) {
                 if (buffer.get() != 0.toByte()) {
                     throw Exception("Inconsistent blob state")
@@ -173,14 +183,12 @@ fun oscPacketToMessage(pkt: ByteArray): OscMessage {
         throw Exception("Ill-formed Message for $address: missing ',' in arguments type tags")
     }
     val args = Array<OscAtomic>(typeTags.value.length - 1) {
-        val tag = typeTags.value[it + 1]
-        when (tag) {
-            'i' -> OscInt(buffer.int)
-            'f' -> OscFloat(buffer.float)
-            's' -> OscString.decode(buffer)
-            'b' -> OscBlob.decode(buffer)
-            else -> throw Exception("Unsupported OSC message argument type: '$tag'")
-        }
+        OscAtomic.decode(typeTags.value[it + 1], buffer)
     }
     return OscMessage(address, args)
+}
+
+private fun alignUp(len: Int): Int {
+    val rem = len % 4
+    return if (rem == 0) len else len + 4 - rem
 }
